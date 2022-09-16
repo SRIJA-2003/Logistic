0ede93d1dd07499572fdde595cb9952ac9ee4eac
SRIJA-2003 <113287988+SRIJA-2003@users.noreply.github.com>
2022-09-16 11:14:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ c=st.number_input('Enter petal width')
 d=st.number_input('Enter petal length')
 result=''
 
-if model.predict('PREDICT'):
+if st.button('PREDICT'):
    result=st.predict([[a,b,c,d]])
    st.success(result)
 
